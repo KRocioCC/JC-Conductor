8c06499dc3935f13165711f01a117284dba78256
Karen Calderon <krcatari@umsa.bo>
2026-08-02 20:30:57 -0400

    creando AppDataBase

---
+++ app/src/main/java/com/karenkotlin/jcconductor/room/AppDataBase.kt
@@ -0,0 +1,18 @@
+package com.karenkotlin.jcconductor.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.karenkotlin.jcconductor.room.dao.AutoDao
+import com.karenkotlin.jcconductor.room.dao.ConductorDao
+import com.karenkotlin.jcconductor.room.entity.Auto
+import com.karenkotlin.jcconductor.room.entity.Conductor
+
+@Database(
+    entities = [Conductor::class,Auto::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun ConductorDao(): ConductorDao
+    abstract fun AutoDao(): AutoDao
+
+}
